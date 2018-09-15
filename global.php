65b746a8cb2df8eef0dd61924b26dd5c70320f92
ChillerDragon <ChillerDragon@gmail.com>
2018-09-15 19:07:54 +0200

    Fix player db path

--- global.php
+++ global.php
@@ -13,9 +13,8 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 //paths
-const DATABASE_PATH = "sqlite:../Not/Done/Yet.db";
 const ABSOLUTE_DATABASE_PATH = "sqlite:/home/chiller/ddpp_database/accounts.db";
-const PLAYER_DATABASE = "sqlite:/var/www/html/hash/tw/players/TeeworldsPlayers.db";
+const PLAYER_DATABASE = "sqlite:/var/www/html/DDNet-web/players/TeeworldsPlayers.db";
 
 //config
 const IS_MINER = false;
