6887e29e5735c045c1af3b970b1eab7439ee8875
ChillerDragon <ChillerDragon@gmail.com>
2018-09-30 01:09:11 +0200

    Add the WIP skins page

---
+++ skins.php
@@ -0,0 +1,25 @@
+<?php
+require_once(__DIR__ . "/global.php");
+session_start();
+if (IS_MINER == true)
+{
+    StartMiner();
+}
+
+?>
+<div class="jungle-background"></div>
+<div class="jungle-mid-light-green"></div>
+<?php
+
+HtmlHeader("Server", "jungle.css");
+?>
+		<h1>Teeworlds skins</h1>
+		<a>
+			some</br>
+			text</br>
+			about</br>
+			skins</br>
+            TODO: use github api to fetch some mapres and skins into this page</br>
+		</a>
+        <a href="https://raw.githubusercontent.com/ChillerDragon/TeeworldsDB/master/gameskins/game_chiller.png"><img src="https://raw.githubusercontent.com/ChillerDragon/TeeworldsDB/master/gameskins/game_chiller.png"></a>
+<?php fok(); ?>
