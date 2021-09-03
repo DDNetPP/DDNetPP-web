1cf6eef852b2ce729311d1f36bc7d50d0468b3e7
ChillerDragon <ChillerDragon@gmail.com>
2021-09-03 19:11:33 +0200

    Add favicon using favicon.io (closed #6)

--- usefull_functions.php
+++ usefull_functions.php
@@ -73,6 +73,10 @@ function HtmlHeader($page, $style = "js_clouds.css", $style2 = "")
 <!DOCTYPE html>
 <html>
 	<head>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/site.webmanifest">
         <link rel="stylesheet" href="design/style.css"></style>
 		<link rel="stylesheet" href="<?php echo "design/$style"; ?>"></style>
 <?php
