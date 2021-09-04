07a23a592c07587fc91d99d30ff0a1c60c879048
ChillerDragon <ChillerDragon@gmail.com>
2021-09-04 10:52:01 +0200

    Start cleaning up the econ related code

--- rcon_api/tw_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "[rcon]:";
-$shellout= shell_exec("./cb5_rcon.exp");
-echo( '<pre>' );
-echo( $shellout );
-echo( '</pre>' );
-
-echo "<input type=\"button\" value=\"back\" onclick=\"window.location.href='index.html'\" />";
-?>
